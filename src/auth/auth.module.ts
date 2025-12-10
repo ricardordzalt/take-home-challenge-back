20413596f8a0be8cd160b93083075294a9be2785
rikardo150 <zlayerz.dev@gmail.com>
2025-12-09 21:58:22 -0700

    added hash service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { RegisterController } from './register/register.controller';
 import { LoginController } from './login/login.controller';
 import { RegisterService } from './register/register.service';
 import { LoginService } from './login/login.service';
+import { HashService } from './shared/hash.service';
+import { TokenService } from './shared/token.service';
 
 @Module({
   controllers: [RegisterController, LoginController],
-  providers: [RegisterService, LoginService]
+  providers: [
+    RegisterService,
+    LoginService,
+    HashService,
+    TokenService,
+  ]
 })
 export class AuthModule { }
---
+++ src/auth/shared/hash.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common/decorators';
+import { hash, compare } from 'bcrypt';
+
+type Hash = {
+    password: string;
+}
+
+type Compare = {
+    password: string;
+    hashed: string;
+}
+
+@Injectable()
+export class HashService {
+    async hash({ password }: Hash) {
+        const saltOrRounds = 10;
+        const hashed = await hash(password, saltOrRounds);
+        return hashed;
+    }
+
+    async compare({ password, hashed }: Compare) {
+        return await compare(password, hashed);
+    }
+}
