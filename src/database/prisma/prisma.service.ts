8ac1ebb5cc0baa39d9e82cb0b8f24dad41b60dfa
rikardo150 <zlayerz.dev@gmail.com>
2025-12-14 13:11:17 -0700

    updated env import on prisma service

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -2,12 +2,13 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    constructor() {
+    constructor(private readonly configService: ConfigService) {
         const pool = new Pool({
-            connectionString: process.env.DATABASE_URL,
+            connectionString: configService.get<string>('DATABASE_URL'),
         })
 
         super({
