7f0a823ecc9551fabd97a1a45cf0fdf33b82f77a
rikardo150 <zlayerz.dev@gmail.com>
2025-12-13 22:39:58 -0700

    added nestjs config for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, DatabaseModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    DatabaseModule,
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
