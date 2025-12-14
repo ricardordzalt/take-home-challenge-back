5e5893d9d651176ce908480f44a8dae51f8dc18e
rikardo150 <zlayerz.dev@gmail.com>
2025-12-14 12:15:40 -0700

    added token service

--- src/auth/shared/token/token.service.ts
+++ src/auth/shared/token/token.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import {
+    JwtService,
+    JwtSignOptions,
+    JwtVerifyOptions
+} from '@nestjs/jwt';
+
+type GenerateToken = {
+    payload: any;
+    options?: JwtSignOptions;
+}
+
+type VerifyToken = {
+    token: string;
+    options?: JwtVerifyOptions;
+}
 
 @Injectable()
-export class TokenService {}
+export class TokenService {
+    constructor(private readonly jwtService: JwtService) { }
+
+    generateToken({ payload, options }: GenerateToken) {
+        return this.jwtService.sign(payload, options);
+    }
+
+    verifyToken({ token, options }: VerifyToken) {
+        return this.jwtService.verify(token, options);
+    }
+}
