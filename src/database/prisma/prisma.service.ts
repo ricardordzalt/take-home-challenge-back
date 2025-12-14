b1d7ba8603bb7ea7c628e8de55abe75406f8e834
rikardo150 <zlayerz.dev@gmail.com>
2025-12-13 22:13:23 -0700

    added prisma service for db connection

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -1,5 +1,34 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 @Injectable()
-export class PrismaService extends PrismaClient { }
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+        })
+
+        super({
+            adapter: new PrismaPg(pool),
+            log: ['query', 'error', 'warn'],
+        })
+    }
+
+    async onModuleInit() {
+        try {
+            await this.$connect()
+        } catch (error) {
+            console.error(`error connecting to database`, error)
+        }
+    }
+
+    async onModuleDestroy() {
+        try {
+            await this.$disconnect()
+        } catch (error) {
+            console.error(`error disconnecting from database`, error)
+        }
+    }
+}
