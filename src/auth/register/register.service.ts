85c152aff124ae5dc7add9a680cc00829e9d36d0
rikardo150 <zlayerz.dev@gmail.com>
2025-12-13 22:40:40 -0700

    added logic for register user

--- src/auth/register/register.service.ts
+++ src/auth/register/register.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { RegisterDto } from './dto/register.dto';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { HashService } from '../shared/hash/hash.service';
+import { UserRepository } from '../../users/repositories/user.repository';
+
+type RegistrationData = {
+    email: string;
+    password: string;
+}
 
 @Injectable()
 export class RegisterService {
-    async register(payload: RegisterDto) {
-        return payload;
+    constructor(
+        private readonly hashService: HashService,
+        private readonly userRepository: UserRepository,
+    ) { }
+    async register(registrationData: RegistrationData): Promise<void> {
+        const user = await this.userRepository.findByEmail(registrationData.email);
+        if (user) {
+            throw new ConflictException({
+                error: 'USER_ALREADY_EXISTS',
+            });
+        }
+        const { password, email } = registrationData || {};
+        const hashedPassword = await this.hashService.hash({
+            password,
+        });
+
+        await this.userRepository.create({
+            email,
+            password: hashedPassword,
+        });
     }
 }
