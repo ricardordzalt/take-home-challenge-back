0f3dc3a1be0d4d9291275162253dc483c6e56ff8
rikardo150 <zlayerz.dev@gmail.com>
2025-12-14 22:46:09 -0700

    added login controller and service

--- src/auth/login/login.controller.ts
+++ src/auth/login/login.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { LoginService } from './login.service';
+import { LoginDto } from './dto/login.dto';
+import { Public } from '../shared/decorators/public.decorator';
 
 @Controller('auth/login')
-export class LoginController { }
+export class LoginController {
+    constructor(private readonly loginService: LoginService) { }
+
+    @Public()
+    @Post()
+    async login(
+        @Body() loginBody: LoginDto,
+    ) {
+        return this.loginService.login(loginBody);
+    }
+}
--- src/auth/login/login.service.ts
+++ src/auth/login/login.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
+import { UserRepository } from 'src/users/repositories/user.repository';
+import { HashService } from '../shared/hash/hash.service';
+import { TokenService } from '../shared/token/token.service';
+import { ConfigService } from '@nestjs/config';
+
+type LoginData = {
+    email: string;
+    password: string;
+}
 
 @Injectable()
-export class LoginService {}
+export class LoginService {
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly hashService: HashService,
+        private readonly tokenService: TokenService,
+        private readonly configService: ConfigService,
+        private readonly logger: Logger,
+    ) { }
+
+    async login(loginData: LoginData) {
+        const { email, password } = loginData;
+        const user = await this.userRepository.findByEmail(email);
+
+        if (!user) {
+            this.logger.error('User not found');
+            throw new UnauthorizedException('User not found');
+        }
+
+        const isPasswordValid = await this.hashService.compare({
+            password,
+            hashed: user.password as string
+        });
+
+        if (!isPasswordValid) {
+            this.logger.error('Invalid password');
+            throw new UnauthorizedException('Invalid password');
+        }
+
+        const accessTokenPayload = {
+            id: user.id,
+        }
+        const accessTokenOptions = {
+            expiresIn: '7d' as `${number}d`,
+        }
+        const accessToken = this.tokenService.generateToken({
+            payload: accessTokenPayload,
+            options: accessTokenOptions,
+        });
+        this.logger.log("Access granted for user: " + user.email);
+
+        return {
+            access_token: accessToken,
+        };
+    }
+}
