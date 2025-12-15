e61b21dfa3ca77e874feb06ee13a9b381b588296
rikardo150 <zlayerz.dev@gmail.com>
2025-12-14 22:45:56 -0700

    added public decorator to register controller

--- src/auth/register/register.controller.ts
+++ src/auth/register/register.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { RegisterService } from './register.service';
 import { RegisterDto } from './dto/register.dto';
+import { Public } from '../shared/decorators/public.decorator';
 
 @Controller('auth/register')
 export class RegisterController {
     constructor(private readonly registerService: RegisterService) { }
 
+    @Public()
     @Post()
     @HttpCode(HttpStatus.CREATED)
     async register(@Body() registerBody: RegisterDto) {
