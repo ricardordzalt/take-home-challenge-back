8845a535bf9fba8cbebf78e98a79975e81c03c69
rikardo150 <zlayerz.dev@gmail.com>
2025-12-17 11:11:38 -0700

    updated user repository to users repository in databae module, also added notifications repository into it

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,23 @@
-import { Module, Global } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
-import { UserRepository } from '../users/repositories/user.repository';
-import { PrismaUserRepository } from './adapters/prisma-user.repository';
+import {Module, Global} from '@nestjs/common';
+import {PrismaService} from './prisma/prisma.service';
+import {UsersRepository} from '../users/repositories/users.repository';
+import {PrismaUsersRepository} from './adapters/prisma-users.repository';
+import {NotificationsRepository} from '../notifications/repositories/notifications.repository';
+import {PrismaNotificationsRepository} from './adapters/prisma-notifications.repository';
 
 @Global()
 @Module({
-    providers: [
-        PrismaService,
-        {
-            provide: UserRepository,
-            useClass: PrismaUserRepository,
-        },
-    ],
-    exports: [UserRepository, PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UsersRepository,
+      useClass: PrismaUsersRepository,
+    },
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsRepository,
+    },
+  ],
+  exports: [UsersRepository, NotificationsRepository, PrismaService],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
