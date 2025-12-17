a803709137bbce63ac48476894f2c2793f66986f
rikardo150 <zlayerz.dev@gmail.com>
2025-12-17 11:12:30 -0700

    update user prisma repository into users prisma repository

---
+++ src/database/adapters/prisma-users.repository.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@nestjs/common';
+import {PrismaService} from '../prisma/prisma.service';
+import {
+  UsersRepository,
+  User,
+  CreateUserParams,
+} from '../../users/repositories/users.repository';
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateUserParams): Promise<User> {
+    return this.prisma.users.create({
+      data: {
+        email: data.email,
+        password: data.password,
+      },
+    });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.users.findUnique({
+      where: {email},
+    });
+  }
+}
