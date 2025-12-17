ab029a74502ac13812d9f04c10809db4b3b16e86
rikardo150 <zlayerz.dev@gmail.com>
2025-12-17 11:10:21 -0700

    added JwtPayload to user decorator

--- src/auth/shared/decorators/user.decorator.ts
+++ src/auth/shared/decorators/user.decorator.ts
@@ -1,10 +1,14 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {createParamDecorator, ExecutionContext} from '@nestjs/common';
+import type {JwtPayload} from '../interfaces/jwt-payload.interface';
 
 export const User = createParamDecorator(
-    (data: string | undefined, ctx: ExecutionContext) => {
-        const request = ctx.switchToHttp().getRequest();
-        const user = request.user;
+  (
+    data: keyof JwtPayload | undefined,
+    ctx: ExecutionContext,
+  ): JwtPayload | string => {
+    const request = ctx.switchToHttp().getRequest();
+    const user: JwtPayload = request.user;
 
-        return data ? user?.[data] : user;
-    },
+    return data ? user?.[data] : user;
+  },
 );
