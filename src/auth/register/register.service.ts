c18d6833f556a79147bf188597dccd0763f2697b
rikardo150 <zlayerz.dev@gmail.com>
2025-12-17 11:10:52 -0700

    update users repository on register service

--- src/auth/register/register.service.ts
+++ src/auth/register/register.service.ts
@@ -1,33 +1,33 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { HashService } from '../shared/hash/hash.service';
-import { UserRepository } from 'src/users/repositories/user.repository';
+import { UsersRepository } from 'src/users/repositories/users.repository';
 
 type RegistrationData = {
-    email: string;
-    password: string;
-}
+  email: string;
+  password: string;
+};
 
 @Injectable()
 export class RegisterService {
-    constructor(
-        private readonly hashService: HashService,
-        private readonly userRepository: UserRepository,
-    ) { }
-    async register(registrationData: RegistrationData): Promise<void> {
-        const user = await this.userRepository.findByEmail(registrationData.email);
-        if (user) {
-            throw new ConflictException({
-                error: 'USER_ALREADY_EXISTS',
-            });
-        }
-        const { password, email } = registrationData || {};
-        const hashedPassword = await this.hashService.hash({
-            password,
-        });
-
-        await this.userRepository.create({
-            email,
-            password: hashedPassword,
-        });
+  constructor(
+    private readonly hashService: HashService,
+    private readonly usersRepository: UsersRepository,
+  ) { }
+  async register(registrationData: RegistrationData): Promise<void> {
+    const user = await this.usersRepository.findByEmail(registrationData.email);
+    if (user) {
+      throw new ConflictException({
+        error: 'USER_ALREADY_EXISTS',
+      });
     }
+    const { password, email } = registrationData || {};
+    const hashedPassword = await this.hashService.hash({
+      password,
+    });
+
+    await this.usersRepository.create({
+      email,
+      password: hashedPassword,
+    });
+  }
 }
