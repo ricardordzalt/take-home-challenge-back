c87cffb145a09b664dd3d4e23a1fd2e5b4cbf065
rikardo150 <zlayerz.dev@gmail.com>
2025-12-18 13:40:36 -0700

    added notification creation into create notification

--- src/notifications/create-notification/create-notification.service.ts
+++ src/notifications/create-notification/create-notification.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Channel, NotificationsRepository } from '../repositories/notifications.repository';
 
 type User = {
   id: string;
@@ -7,15 +8,42 @@ type User = {
 type createNotificationData = {
   title: string;
   content: string;
-  channel: string;
+  channel: Channel;
+};
+
+type CreateNotificationResponse = {
+  message: string[];
+  statusCode: number;
+  notification: {
+    id: string;
+  };
 };
 
 @Injectable()
 export class CreateNotificationService {
+  constructor(
+    private readonly notificationsRepository: NotificationsRepository
+  ) { }
   async createNotification(
     user: User,
     createNotificationData: createNotificationData,
-  ) {
-    return createNotificationData;
+  ): Promise<CreateNotificationResponse> {
+    const { title, content, channel } = createNotificationData || {};
+    const { id: userId } = user || {};
+    const notificationData = {
+      title,
+      content,
+      channel,
+      userId,
+    };
+    const notification = await this.notificationsRepository.create(notificationData);
+    const response = {
+      message: ['Notification created successfully'],
+      statusCode: 201,
+      notification: {
+        id: notification.id,
+      },
+    }
+    return response;
   }
 }
